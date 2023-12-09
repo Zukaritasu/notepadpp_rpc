679f356342e7430f3a14acd98bbf3d7363cfb511
Ameer Alntsheh <68712253+Abu-Maher@users.noreply.github.com>
2023-12-09 21:19:56 +0300

    Update LanguageInfo.cpp
    Fixing the ASM.

--- vstudio/src/LanguageInfo.cpp
+++ vstudio/src/LanguageInfo.cpp
@@ -62,7 +62,7 @@ LanguageInfo GetLanguageInfo(const char* extension)
 	case L_ERLANG:        return { "ERLANG", "erlang" };
 	case L_COFFEESCRIPT:  return { "COFFEESCRIPT", "coffeescript" };
 	case L_RC:            return { "RESOURCE", NPP_DEFAULTIMAGE };
-	case L_ASSEMBLY:             return { "ASSEMBLY", "assembly" };
+	case L_ASM:             return { "ASSEMBLY", "assembly" };
 	default:
 		if (strcmp(extension, ".gitignore") == 0)
 			return               { "GIT", "git" };
