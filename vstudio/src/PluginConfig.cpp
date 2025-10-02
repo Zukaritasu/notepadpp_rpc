a88ba756aa0029ba8870dbd1a40cdb39fc9758cc
Zukaritasu <perezjhonlenno29@gmail.com>
2025-10-01 23:37:46 -0400

    Updated file header comments

--- vstudio/src/PluginConfig.cpp
+++ vstudio/src/PluginConfig.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2022 Zukaritasu
+// Copyright (C) 2022 - 2025 Zukaritasu
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
